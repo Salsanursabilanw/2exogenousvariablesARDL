029407672f6be817441264266dc7ff1773ef29bc
Salsa Nursabila Nanda Winarno <74189753+Salsanursabilanw@users.noreply.github.com>
2022-03-31 16:00:26 +0700

    Add files via upload

---
+++ 2 exogenous variables ARDL.R
@@ -0,0 +1,48 @@
+print("Hello")
+#let's import the data
+library(readr)
+data <- read_delim("datav.csv", delim = ";", 
+                      escape_double = FALSE, trim_ws = TRUE)
+view(datva)
+attach(datav)
+str(datav)
+datav = ts(datav, start=c(2010,1), frequency=12)
+plot(datav) #let's make time series plot of all variables
+#load libraries we need
+library(tseries)
+library(ARDL)
+library(lmtest)
+library(car)
+library(strucchange)
+#Stationarity Test
+pp.test(y)
+pp.test(x1)
+pp.test(x2)
+#Stationarity test for first difference
+pp.test(diff(y)) #Stationary
+pp.test(diff(x1)) #Stationary
+pp.test(diff(x2)) #Stationary
+#ARDL Model
+models = auto_ardl(y ~ x1 + x2, data= datav, max_order =5)
+summary(models)
+#Optimum Lag
+models$top_order
+#Building model with the best lag order
+modelardl1 = models$best_model
+summary(modelardl)
+#cointegration test(Long-run levels relationship)
+fbounds = bounds_f_test(modelardl, case= 3, alpha = 0.05)
+fbounds$tab
+uecmardl=uecm(modelardl1)
+tbounds = bounds_t_test(uecmardl, case=3, alpha=0.01)
+tbounds$tab #there is no cointegration
+#assumption check
+#autocorrelation test
+bgtest(modelardl1)
+#multicolinearity test
+bptest(modelardl1)
+#heteroskedastisity test
+vif(modelardl1)
+#Model Stability Test
+modelt=efp(y ~ x1 + x2)
+plot(modelt)
